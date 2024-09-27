dcf21bd7d23cc328f52b514a2f4481d47402bdf3
dlovell <dlovell@gmail.com>
2024-09-27 12:08:47 -0400

    feat: add app `nix-flake-metadata-refresh` to refresh own flake metadata

--- flake.nix
+++ flake.nix
@@ -1,17 +1,37 @@
 {
   description = "A template for using nix-direnv and poetry2nix";
 
-  outputs = { self }: {
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: {
+    # templates must not live insides eachDefaultSystem
     templates = {
       poetry2nix-direnv = {
         path = ./poetry2nix-direnv;
         description = "Application packaged using nix-direnv and poetry2nix";
         welcomeText = ''
-          You just create a poetry2nix-direnv template
+          You just created a poetry2nix-direnv template
         '';
       };
       default = self.templates.poetry2nix-direnv;
-      defaultTemplate = self.templates.poetry2nix-direnv;
     };
-  };
+  } // (flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      nix-flake-metadata-refresh = pkgs.writeShellScriptBin "nix-flake-metadata-refresh" ''
+        ${pkgs.nix}/bin/nix flake metadata --refresh github:dlovell/poetry2nix-direnv
+      '';
+    in
+    {
+      apps = {
+        nix-flake-metadata-refresh = {
+          type = "app";
+          program = "${nix-flake-metadata-refresh}/bin/nix-flake-metadata-refresh";
+        };
+        default = self.apps.${system}.nix-flake-metadata-refresh;
+      };
+    }));
 }
