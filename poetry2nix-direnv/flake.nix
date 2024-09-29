f6aa5b5a3aea8a4eee54aa74de3e8e9870039777
dlovell <dlovell@gmail.com>
2024-09-29 10:03:07 -0400

    feat: use letsql/nix-utils to provide default app of python-with-app

--- poetry2nix-direnv/flake.nix
+++ poetry2nix-direnv/flake.nix
@@ -8,9 +8,13 @@
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-utils = {
+      url = "github:letsql/nix-utils/develop";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, nix-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -19,13 +23,19 @@
             poetry2nix.overlays.default
           ];
         };
+        utils = nix-utils.lib.${system}.mkUtils { inherit pkgs; };
         inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
         myapp = mkPoetryApplication {
           projectDir = ./.;
           preferWheels = true;
         };
-      in
-      {
+        mypython = myapp.python.withPackages (_: myapp.requiredPythonModules ++ [ myapp ]);
+      in {
+        apps = {
+          # nix-flake-metadata-refresh = utils.mkNixFlakeMetadataRefresh "github:myuser/this-repo";
+          mypython = utils.drvToApp { drv = mypython; name = "python"; };
+          default = self.apps.${system}.mypython;
+        };
         packages = {
           inherit myapp;
           default = self.packages.${system}.myapp;
